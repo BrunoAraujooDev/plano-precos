58ab89018d42631731a681836481cc8b4a9c7cae
Bruno Araujo <bruno.aoliveira@al.infnet.edu.br>
2022-03-08 22:45:16 -0300

    colocando o snackbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cardPrecos';
+  constructor(private _snackBar: MatSnackBar) {}
 
   lista1: string[] = ["500 GB Storage", "2 Users Allowed", "Send up to 3 GB"];
   lista2: string[] = ["1 TB Storage", "5 Users Allowed", "Send up to 10 GB"];
   lista3: string[] = ["2 TB Storage", "10 Users Allowed", "Send up to 20 GB"];
 
-  alertaCompra():void{
-    alert("Pacote comprado!");
+  
+  openSnackBar(){
+    this._snackBar.open("Pacote Comprado", "X")
   }
+  
 }
